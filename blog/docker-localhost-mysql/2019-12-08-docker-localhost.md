7edccd6cb71441319c18515188006423f02d2afc
ndo@ndo1 <yo@ni.co.de>
2020-03-06 23:38:49 +0100

    initial commit ✨

---
+++ blog/docker-localhost-mysql/2019-12-08-docker-localhost.md
@@ -0,0 +1,25 @@
+---
+date: "2019-12-08"
+title:  "Docker + Localhost MySQL"
+tags: ["servers", "linux", "mysql", "docker"]
+---
+
+I was having the hardest time getting docker containers to connect to the hosts instance of mysql. 
+
+I finally found a great solution, so I wanted to post it here for myself and others in the future:
+
+Enable `route_localnet` for docker0 interface:
+
+`$ sysctl -w net.ipv4.conf.docker0.route_localnet=1`
+
+Add this rules to iptables:
+
+`$ iptables -t nat -I PREROUTING -i docker0 -d 172.17.0.1 -p tcp --dport 3306 -j DNAT --to 127.0.0.1:3306`
+
+`$ iptables -t filter -I INPUT -i docker0 -d 127.0.0.1 -p tcp --dport 3306 -j ACCEPT`
+
+Create mysql user with access from '%' that mean - from anyone, excluding localhost:
+
+`CREATE USER 'user'@'%' IDENTIFIED BY 'password';`
+
+Change in your docker run command or docker-compose, etc the mysql-server address to 172.17.0.1
---
+++ config/SiteConfig.ts
@@ -0,0 +1,36 @@
+export default {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+
+  siteTitle: 'Nico Domino', // Navigation and Site Title
+  siteTitleAlt: 'Nico Domino - 2020', // Alternative Site title for SEO
+  siteUrl: 'https://next.ndo.dev', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteBanner: '/assets/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
+  defaultBg: '/assets/bg.png', // default post background header
+  favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
+  siteDescription: 'Typescript Power Blog with big typography', // Your site description
+  author: 'Nico Domino', // Author for schemaORGJSONLD
+  siteLogo: '/assets/logo.png', // Image for schemaORGJSONLD
+
+  userTwitter: '@ndom91', // Twitter Username - Optional
+  userGithub: '@ndom91', // Github Username - Optional
+  ogSiteName: 'ndom91', // Facebook Site Name - Optional
+  ogLanguage: 'en_US', // Facebook Language
+
+  // Manifest and Progress color
+  // See: https://developers.google.com/web/fundamentals/web-app-manifest/
+  themeColor: '#3498DB',
+  backgroundColor: '#2b2e3c',
+
+  // Settings for typography.ts
+  headerFontFamily: 'Bitter',
+  bodyFontFamily: 'Open Sans',
+  baseFontSize: '18px',
+
+  // Social media
+  siteFBAppID: '',
+
+  //
+  Google_Tag_Manager_ID: 'GTM-XXXXXXX',
+  POST_PER_PAGE: 4,
+}
