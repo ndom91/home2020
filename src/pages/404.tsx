193eb8633096834a64157130c77e8adc9a72620c
ndo@ndo1 <yo@ndo.dev>
2020-03-15 13:09:29 +0100

    add: 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,34 @@
-import * as React from 'react';
-import { Content, Header, Layout, Wrapper, SectionTitle } from '../components';
-import Helmet from 'react-helmet';
-import config from '../../config/SiteConfig';
-import { Link } from 'gatsby';
+import * as React from 'react'
+import styled from 'styled-components'
+import { Content, Header, Layout, Wrapper, SectionTitle } from '../components'
+import Helmet from 'react-helmet'
+import config from '../../config/SiteConfig'
+import { Link } from 'gatsby'
+
+const HeaderText = styled.h1`
+  text-align: center;
+  font-size: 6rem;
+  font-family: ${config.serifFontFamily};
+  margin-bottom: 20px;
+`
+
+const Image = styled.img`
+  max-width: 600px;
+`
+
+const ImageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 75px 0;
+`
+
+const Subtitle = styled.h3`
+  text-align: center;
+  font-size: 2rem;
+  font-weight: 100;
+  font-family: ${config.serifFontFamily};
+  margin-bottom: 50px;
+`
 
 export default class NotFoundPage extends React.Component<any> {
   public render() {
@@ -15,10 +41,14 @@ export default class NotFoundPage extends React.Component<any> {
         </Header>
         <Wrapper>
           <Content>
-            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+            <HeaderText>404</HeaderText>
+            <ImageWrapper>
+              <Image src="assets/images/404.svg" alt="404 - Not Found" />
+            </ImageWrapper>
+            <Subtitle>You just hit a route that doesn&#39;t exist... the sadness.</Subtitle>
           </Content>
         </Wrapper>
       </Layout>
-    );
+    )
   }
 }
